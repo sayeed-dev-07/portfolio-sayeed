edca8978d8138d27f78ee9a972067544c5718f66
sayeed <expsayeedshorif@gmail.com>
2026-02-14 20:06:57 +0600

    intial project setup done and done ssome gif animation experiment

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/heroComponents/navbar";
+
+export default function Home() {
+  return (
+    <div className="w-full bg-foreground min-h-screen">
+      <Navbar/>
+    </div>
+  );
+}
