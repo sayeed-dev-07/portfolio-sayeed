59f679ff48fbf9370fbedebf920298355bfd7e71
sayeed <expsayeedshorif@gmail.com>
2026-02-19 19:01:34 +0600

    added some photots and delte some from gallery

--- public/data/imgData.ts
+++ public/data/imgData.ts
@@ -1,7 +1,8 @@
 export const galleryData = [
-
+    '/img/1.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
     '/img/6.jpg',
-    '/img/7.jpg',
     '/img/9.jpg',
-    '/img/10.jpg',
 ]
